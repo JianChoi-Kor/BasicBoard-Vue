bd60b50ff97e782711eec84dddecd90e152d2b5a
JianChoi-Kor <wldks79@gmail.com>
2022-07-03 02:01:36 +0900

    vue

---
+++ src/views/1_databinding/DataBindingHtmlView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+  return {
+      sampleData:''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  methods:{}
+}
+</script>
